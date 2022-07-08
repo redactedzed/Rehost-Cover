7f81f52c3e29fee0706b46e6fb00c8a5c5fdc9d1
spinfast319 <spinfast319@protonmail.com>
2022-07-07 23:46:54 -0400

    added note that example API keys are artificial

--- test-config.py
+++ test-config.py
@@ -22,7 +22,7 @@ c_log_directory = "M:\Music\Logs" # Which directory do you want the log albums t
 # Set your site and API information here
 c_site_ajax_page = "https://redacted.ch/ajax.php?action=groupedit&id=" # set the gazelle ajax page here
 c_site_collage_ajax_page = "https://redacted.ch/ajax.php?action=addtocollage&collageid=" # set the gazelle collage ajax page here
-c_r_api_key = "d64bq2l9.t256n539742x33120h9s5623h3615471" # set your RED api key here
-c_p_api_key = "g56643k-35h7-3d1d-5sf5-69h22562q992" # set your ptpIMG api key here
+c_r_api_key = "d64bq2l9.t256n539742x33120h9s5623h3615471" # set your RED api key here, this key is fake
+c_p_api_key = "g56643k-35h7-3d1d-5sf5-69h22562q992" # set your ptpIMG api key here, this key is fake
 '''
 
